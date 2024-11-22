c6ffb589fb771a8be7d95d595b5999bd2b3489c9
Dio <144291224+DiegoOvidio@users.noreply.github.com>
2024-11-21 22:02:55 -0300

    Add files via upload

---
+++ SI_T2/app.py
@@ -0,0 +1,22 @@
+# Conjunto de dados
+filmes = [
+    {"nome": "Mad Max", "genero": "Ação"},
+    {"nome": "Toy Story", "genero": "Comédia"},
+    {"nome": "Invocação do Mal", "genero": "Terror"},
+    {"nome": "O Rei Leão", "genero": "Animação"},
+    {"nome": "The bad guys", "genero": "Animação"},
+    {"nome": "Seasaw", "genero": "Terror"},
+    {"nome": "Sonic", "genero": "Comédia"},
+]
+
+# Entrada do usuário
+genero_preferido = input("Qual gênero você prefere (Ação, Comédia, Terror, Animação)? ").capitalize()
+
+# Filtrar filmes
+sugestoes = [f['nome'] for f in filmes if f['genero'] == genero_preferido]
+
+# Exibir sugestões
+if sugestoes:
+    print(f"Recomendamos os seguintes filmes de {genero_preferido}: {', '.join(sugestoes)}")
+else:
+    print("Desculpe, não temos sugestões para esse gênero.")
